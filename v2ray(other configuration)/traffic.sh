96e4f1a46fae7de76a15c2a38554a6c6ff45fe41
lxhao61 <lxhao61@gmail.com>
2021-03-28 08:14:41 +0800

    Create traffic.sh

---
+++ v2ray(other configuration)/traffic.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+_APISERVER=127.0.0.1:10085 //此端口必须与v2ray config.json中流量统计端口一致
+_V2CTL=/usr/local/bin/v2ray/v2ctl //此路径为v2ray的v2ctl实际路径
+
+apidata () {
+    local ARGS=
+    if [[ $1 == "reset" ]]; then
+      ARGS="reset: true"
+    fi
+    $_V2CTL api --server=$_APISERVER StatsService.QueryStats "${ARGS}" \
+    | awk '{
+        if (match($1, /name:/)) {
+            f=1; gsub(/^"|link"$/, "", $2);
+            split($2, p,  ">>>");
+            printf "%s:%s->%s\t", p[1],p[2],p[4];
+        }
+        else if (match($1, /value:/) && f){ f = 0; printf "%.0f\n", $2; }
+        else if (match($0, /^>$/) && f) { f = 0; print 0; }
+    }'
+}
+
+print_sum() {
+    local DATA="$1"
+    local PREFIX="$2"
+    local SORTED=$(echo "$DATA" | grep "^${PREFIX}" | sort -r)
+    local SUM=$(echo "$SORTED" | awk '
+        /->up/{us+=$2}
+        /->down/{ds+=$2}
+        END{
+            printf "SUM->up:\t%.0f\nSUM->down:\t%.0f\nSUM->TOTAL:\t%.0f\n", us, ds, us+ds;
+        }')
+    echo -e "${SORTED}\n${SUM}" \
+    | numfmt --field=2 --suffix=B --to=iec \
+    | column -t
+}
+
+DATA=$(apidata $1)
+echo "------------Inbound----------"
+print_sum "$DATA" "inbound"
+echo "-----------------------------"
+echo
+echo "-------------User------------"
+print_sum "$DATA" "user"
+echo "-----------------------------"
